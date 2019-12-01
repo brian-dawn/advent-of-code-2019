755b4003819d06c94317b9d7444e28c866aad324
Brian Dawn <brian.t.dawn@gmail.com>
2019-12-01 07:49:45 -0600

    Add day1.

---
+++ src/bin/day1.rs
@@ -0,0 +1,55 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::iter::Sum;
+
+fn read_input() -> Option<Vec<i32>> {
+    let input = File::open("input/day1.txt").ok()?;
+    let buffered = BufReader::new(input);
+
+    buffered
+        .lines()
+        .map(|line| Some(line.ok()?.parse::<i32>().ok()?))
+        .collect()
+}
+
+fn compute_fuel(mass: i32) -> i32 {
+    mass / 3 - 2
+}
+
+#[test]
+fn test_compute_fuel() {
+    assert_eq!(654, compute_fuel(1969));
+    assert_eq!(33583, compute_fuel(100756));
+}
+
+fn compute_fuel_fuel(mass: i32, sum: i32) -> i32 {
+    let cost = compute_fuel(mass);
+    if cost > 0 {
+        return compute_fuel_fuel(cost, sum + cost);
+    }
+    return sum;
+}
+
+#[test]
+fn test_compute_fuel_fuel() {
+    assert_eq!(50346, compute_fuel_fuel(100756, 0));
+}
+
+fn main() {
+    let input = read_input();
+    let day1: i32 = read_input()
+        .expect("failed to load input")
+        .iter()
+        .map(|i| compute_fuel(*i))
+        .sum();
+
+    println!("day1: {}", day1);
+
+    let day2: i32 = read_input()
+        .expect("failed to load input")
+        .iter()
+        .map(|i| compute_fuel_fuel(*i, 0))
+        .sum();
+
+    println!("day2: {}", day2);
+}
