bbef7bb4ed23ee8612a1b5221fc4b48c28c32995
Brian Dawn <brian.t.dawn@gmail.com>
2019-12-02 14:02:52 -0600

    Add a test.

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -10,7 +10,7 @@ fn read_input() -> Result<Vec<usize>> {
         .collect()
 }
 
-fn run(noun: usize, verb: usize, data: &[usize]) -> Option<usize> {
+fn run(noun: usize, verb: usize, data: &[usize]) -> Option<Vec<usize>> {
     let mut program = data.to_owned();
     program[1] = noun;
     program[2] = verb;
@@ -30,19 +30,33 @@ fn run(noun: usize, verb: usize, data: &[usize]) -> Option<usize> {
         }
     }
 
-    Some(program[0])
+    Some(program)
+}
+
+#[test]
+fn test_run() {
+    let test = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+    let result = run(9, 10, &test);
+    assert_eq!(
+        vec!(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50),
+        result.unwrap()
+    );
+}
+
+fn solve(noun: usize, verb: usize, data: &[usize]) -> Option<usize> {
+    run(noun, verb, data).map(|program| program[0])
 }
 
 fn main() -> Result<()> {
     let input = read_input()?;
 
-    let part1 = run(12, 2, &input).context("failed to find a solution")?;
+    let part1 = solve(12, 2, &input).context("failed to find a solution")?;
     println!("part1: {}", part1);
 
     let val = 19_690_720;
     for noun in 0..100 {
         for verb in 0..100 {
-            let result = run(noun, verb, &input);
+            let result = solve(noun, verb, &input);
             if result == Some(val) {
                 println!("part2: {}", 100 * noun + verb);
                 return Ok(());
