d5349a66c4e1572140aedec9903d50e614c8b45f
Brian Dawn <brian.t.dawn@gmail.com>
2019-12-11 09:09:42 -0600

    Add day10 part 2.

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate anyhow;
-
 use anyhow::{Context, Result};
+use itertools::Itertools;
+use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 use std::fs;
@@ -56,10 +58,17 @@ fn parse(data: &str) -> Result<Space> {
 fn angle(p1: Point, p2: Point) -> f64 {
     let y = p2.y as f64 - p1.y as f64;
     let x = p2.x as f64 - p1.x as f64;
-    let angle = y.atan2(x) * 180f64 / std::f64::consts::PI;
+    let angle = y.atan2(x) * 180f64 / std::f64::consts::PI + 90f64;
 
     let dec = 1_000_000_000_f64;
     (angle * dec).round() / dec
+}
+
+fn distance(p1: Point, p2: Point) -> f64 {
+    let x = p1.x as f64 - p2.x as f64;
+    let y = p1.y as f64 - p2.y as f64;
+
+    (x * x + y * y).sqrt()
 }
 
 fn visible_points(our_point: Point, space: &Space) -> usize {
@@ -75,7 +84,7 @@ fn visible_points(our_point: Point, space: &Space) -> usize {
         .len()
 }
 
-fn part1(input: &str) -> Result<usize> {
+fn part1(input: &str) -> Result<(Point, usize)> {
     let parsed = parse(input)?;
 
     let best_count = parsed
@@ -90,25 +99,92 @@ fn part1(input: &str) -> Result<usize> {
                 Some((_, best_count)) => {
                     //
                     if best_count < count {
-                        Some((point, count))
+                        Some((*point, count))
                     } else {
                         best
                     }
                 }
-                None => Some((point, count)),
+                None => Some((*point, count)),
             }
         })
-        .context("no valid points")?
-        .1;
+        .context("no valid points")?;
 
     Ok(best_count)
+}
+
+fn part2(input: &str, laser: Point) -> Result<Vec<Point>> {
+    let parsed = parse(input)?;
+
+    let dec = 1_000_000_000_f64;
+    let mut world: Vec<(i64, (i64, Point))> = parsed
+        .asteroids
+        .iter()
+        .map(|asteroid| {
+            // first one is the closest
+            let angle = angle(laser, *asteroid);
+            // 0 degrees needs to be the smallest angle.
+            let angle = if angle < 0f64 { 360f64 + angle } else { angle };
+            let distance = distance(laser, *asteroid) * dec;
+            ((dec * angle) as i64, (distance as i64, *asteroid))
+        })
+        .collect();
+
+    let num_asteroids = world.len();
+    let mut by_angle = world.into_iter().into_group_map();
+    let mut angles: Vec<i64> = by_angle.iter().map(|v| *v.0).collect();
+
+    angles.sort();
+
+    let mut blown_up = HashSet::new();
+    let mut order = Vec::new();
+    loop {
+        for angle in &angles {
+            let distance_points = &by_angle[&angle];
+            let mut dist_points = distance_points.clone();
+            dist_points.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap()); // Sort by distance.
+
+            for (distance, point) in dist_points {
+                if !blown_up.contains(&point) {
+                    blown_up.insert(point);
+                    order.push(point);
+                    break;
+                }
+            }
+        }
+        if order.len() >= num_asteroids {
+            break;
+        }
+    }
+
+    Ok(order)
 }
 
 fn main() -> Result<()> {
     let contents = fs::read_to_string("input/day10.txt")?;
 
-    let p1 = part1(&contents)?;
-    println!("{}", p1);
+    let laser = part1(&contents)?;
+    println!("{}", laser.1);
+
+    let order = part2(&contents, laser.0)?;
+    let asteroid = order[199];
+    println!("{}", asteroid.x * 100 + asteroid.y);
+
+    Ok(())
+}
+
+#[test]
+fn test_part2() -> Result<()> {
+    let example = ".#....#####...#..
+                   ##...##.#####..##
+                   ##...#...#.#####.
+                   ..#.........###..
+                   ..#.#.....#....##";
+
+    let result = part2(example, Point::new(8, 3))?;
+
+    assert_eq!(Point::new(8, 1), result[0]);
+    assert_eq!(Point::new(9, 0), result[1]);
+    assert_eq!(Point::new(9, 1), result[2]);
 
     Ok(())
 }
@@ -121,7 +197,7 @@ fn test_part1() -> Result<()> {
                          ....#
                          ...##";
 
-    assert_eq!(8, part1(example)?);
+    assert_eq!(8, part1(example)?.1);
 
     Ok(())
 }
@@ -142,12 +218,14 @@ fn test_visible_points() -> Result<()> {
 }
 #[test]
 fn test_angle() {
-    assert_eq!(0f64, angle(Point::new(2, 2), Point::new(3, 2)));
-    assert_eq!(45f64, angle(Point::new(2, 2), Point::new(3, 3)));
-    assert_eq!(45f64, angle(Point::new(2, 2), Point::new(4, 4)));
-    assert_eq!(90f64, angle(Point::new(2, 2), Point::new(2, 3)));
-    assert_eq!(-45f64, angle(Point::new(2, 2), Point::new(3, 1)));
-    assert_eq!(-135f64, angle(Point::new(2, 2), Point::new(1, 1)));
+    assert_eq!(0f64, angle(Point::new(2, 2), Point::new(2, 1)));
+
+    assert_eq!(90f64, angle(Point::new(2, 2), Point::new(3, 2)));
+    assert_eq!(135f64, angle(Point::new(2, 2), Point::new(3, 3)));
+    assert_eq!(135f64, angle(Point::new(2, 2), Point::new(4, 4)));
+    assert_eq!(180f64, angle(Point::new(2, 2), Point::new(2, 3)));
+    assert_eq!(45f64, angle(Point::new(2, 2), Point::new(3, 1)));
+    assert_eq!(-45f64, angle(Point::new(2, 2), Point::new(1, 1)));
 }
 
 #[test]
